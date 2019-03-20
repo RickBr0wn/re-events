55ff21ef8e39d9e9d10730b5c87dbab737641130
Rick Brown <ricky.brown.00@gmail.com>
2019-03-20 15:44:46 +0000

    Added UserDetail Page (& all associated compos)

---
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import UserDetailedEvents from './UserDetailedEvents'
+import UserDetailedHeader from './UserDetailedHeader'
+import UserDetailedPhotos from './UserDetailedPhotos'
+import UserDetailedSidebar from './UserDetailedSidebar'
+import UserDetailedDescription from './UserDetailedDescription'
+
+const query = ({ auth }) => {
+  return [
+    {
+      collection: 'users',
+      doc: auth.uid,
+      subcollections: [{ collection: 'photos' }],
+      storeAs: 'photos'
+    }
+  ]
+}
+
+class UserDetailedPage extends Component {
+  render() {
+    const { profile, photos } = this.props
+    return (
+      <Grid>
+        <UserDetailedHeader profile={profile} />
+        <UserDetailedDescription profile={profile} />
+        <UserDetailedSidebar />
+        {photos && photos.length > 0 && <UserDetailedPhotos photos={photos} />}
+        <UserDetailedEvents />
+      </Grid>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  profile: state.firebase.profile,
+  auth: state.firebase.auth,
+  photos: state.firestore.ordered.photos
+})
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect(auth => query(auth))
+)(UserDetailedPage)
